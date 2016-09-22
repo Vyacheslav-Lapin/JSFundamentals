4b0882f62a8fbe56033eff07317293709fa9bab4
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-09-22 08:00:01 +0300

    js - 1

--- _scripts/script.js
+++ _scripts/script.js
@@ -1,3 +1,20 @@
 /**
  * Created by admin on 08/09/16.
  */
+
+function m1(x) {
+
+    console.log(arguments[0]);
+    x = 200;
+    console.log(arguments[0]);
+
+    console.log(arguments.length);
+    console.log(m1.length);
+
+    console.log(arguments.length === arguments.callee.length);
+
+    var obj = { /*...*/ };
+    var z = 5;
+}
+
+m1(150);
