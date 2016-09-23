234d54381d8472db72a33f7926ea44ca8a45c4c7
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-09-23 13:38:30 +0300

    js - dom - 2

--- _scripts/script2.js
+++ _scripts/script2.js
@@ -1,6 +1,22 @@
-'use strict';
+// 'use strict';
 
-document.getElementById("avatar").addEventListener(
+//noinspection JSValidateTypes
+let /** @type HTMLElement */ element = document.getElementById("avatar");
+
+element.addEventListener(
     "click",
     event => console.log('Меня зовут Вася!'),
     true);
+
+console.log(element.innerHTML);
+
+// element.innerHTML += "<div>Мама мыла раму!!!</div>";
+
+//noinspection JSValidateTypes
+let /** @type HTMLDivElement */ div = document.createElement("div");
+div.align = "right";
+let /** @type Text */ text = document.createTextNode("Мама мыла раму!!!");
+div.appendChild(text);
+element.appendChild(div);
+
+console.log(element.innerHTML);
