a93a7a4913544e1dd7941601e4d22437365a4369
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-09-29 14:36:01 +0300

    js - webgl - 2.1 - bug fix

--- _scripts/webgl/typedArrays.js
+++ _scripts/webgl/typedArrays.js
@@ -7,8 +7,8 @@ let buffer = new ArrayBuffer(20),
 
     //хранение 8- и 16- раз рядных целых чисел в одном буфере
     int8s = new Int8Array(buffer, 0, 9),
-    uintlбs = new Uint16Array(buffer,
+    uIntL6s = new Uint16Array(buffer,
         int8s.length * int8s.constructor.BYTES_PER_ELEMENT + 1);
 
 console.log(int8s.length * int8s.constructor.BYTES_PER_ELEMENT + 1);
-console.log(uintlбs.length);
+console.log(uIntL6s.length);
