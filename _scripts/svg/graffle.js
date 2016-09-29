141e1067c59291ddf060b5a52c603f6dfec56532
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-09-29 12:35:16 +0300

    js - rafaёl - 4

--- _scripts/svg/graffle.js
+++ _scripts/svg/graffle.js
@@ -1,3 +1,5 @@
+'use strict';
+
 Raphael.fn.connection = function (obj1, obj2, line, bg) {
     if (obj1.line && obj1.from && obj1.to) {
         line = obj1;
@@ -47,7 +49,11 @@ Raphael.fn.connection = function (obj1, obj2, line, bg) {
     } else {
         var color = typeof line == "string" ? line : "#000";
         return {
-            bg: bg && bg.split && this.path(path).attr({stroke: bg.split("|")[0], fill: "none", "stroke-width": bg.split("|")[1] || 3}),
+            bg: bg && bg.split && this.path(path).attr({
+                stroke: bg.split("|")[0],
+                fill: "none",
+                "stroke-width": bg.split("|")[1] || 3
+            }),
             line: this.path(path).attr({stroke: color, fill: "none"}),
             from: obj1,
             to: obj2
@@ -55,31 +61,33 @@ Raphael.fn.connection = function (obj1, obj2, line, bg) {
     }
 };
 
-var el;
-window.onload = function () {
-    var dragger = function () {
+addEventListener("DOMContentLoaded", () => {
+    let connections = [],
+        shapes = [
+            r.ellipse(190, 100, 30, 20),
+            r.rect(290, 80, 60, 40, 10),
+            r.rect(290, 180, 60, 40, 2),
+            r.ellipse(450, 100, 20, 20)
+        ],
+        dragger = function () {
             this.ox = this.type == "rect" ? this.attr("x") : this.attr("cx");
             this.oy = this.type == "rect" ? this.attr("y") : this.attr("cy");
             this.animate({"fill-opacity": .2}, 500);
         },
         move = function (dx, dy) {
-            var att = this.type == "rect" ? {x: this.ox + dx, y: this.oy + dy} : {cx: this.ox + dx, cy: this.oy + dy};
-            this.attr(att);
-            for (var i = connections.length; i--;) {
-                r.connection(connections[i]);
-            }
+            this.attr(
+                this.type == "rect" ?
+                {x: this.ox + dx, y: this.oy + dy} :
+                {cx: this.ox + dx, cy: this.oy + dy}
+            );
+            connections.forEach(connection => r.connection(connection));
             r.safari();
         },
         up = function () {
             this.animate({"fill-opacity": 0}, 500);
         },
-        r = Raphael("holder", 640, 480),
-        connections = [],
-        shapes = [  r.ellipse(190, 100, 30, 20),
-            r.rect(290, 80, 60, 40, 10),
-            r.rect(290, 180, 60, 40, 2),
-            r.ellipse(450, 100, 20, 20)
-        ];
+        r = Raphael("holder", 640, 480);
+
     for (var i = 0, ii = shapes.length; i < ii; i++) {
         var color = Raphael.getColor();
         shapes[i].attr({fill: color, stroke: color, "fill-opacity": 0, "stroke-width": 2, cursor: "move"});
@@ -88,4 +96,5 @@ window.onload = function () {
     connections.push(r.connection(shapes[0], shapes[1], "#fff"));
     connections.push(r.connection(shapes[1], shapes[2], "#fff", "#fff|5"));
     connections.push(r.connection(shapes[1], shapes[3], "#000", "#fff"));
-};
+
+}, true);
