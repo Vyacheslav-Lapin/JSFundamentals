476ef9fe8c9805696c50d2e36348f8380e4c1225
Vyacheslav-Lapin <Vyachesl@vLapin.ru>
2016-10-07 13:31:17 +0300

    js - vuejs - 1

--- _scripts/vue/example.js
+++ _scripts/vue/example.js
@@ -1,13 +1,16 @@
 "use strict";
 // import * as Vue from "vue/src/shared/util"; // require("vue/types/vue");
 
-// addEventListener("DOMContentLoaded", () => {
-    const Todos = Vue.extend({
-        name: `todos`
-    });
+const Todos = Vue.extend({
+    name: `todos`
+});
 
-    const vm = new Todos({
-        el: `#todos`,
-        ready: () => console.log('ViewModel ready!')
-    });
-// }, true);
+const vm = new Todos({
+    // el: `#todos`,
+    data: {
+        title: 'Todos'
+    },
+    ready: () => console.log('ViewModel ready!')
+});
+
+vm.$mount('#todos');
